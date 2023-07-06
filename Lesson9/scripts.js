a55212364c36d8857e64b533691ccaa42423bd38
DUNGPHPANDREACT <dung2000cn@gmail.com>
2023-07-06 19:25:40 +0700

    for in object

--- Lesson9/scripts.js
+++ Lesson9/scripts.js
@@ -21,15 +21,43 @@ const listNumber = [1, 2, 3];
 // console.log(computer['0'])
 
 // Update
-const student = {
-	name: 'Nguyễn Văn A',
-    gender: 'girl'
-};
-console.log(student);
-student['name'] = 'Trần Thị B';
-//student.name = 'Trần Thị B';
-student.age = 15;
-console.log(student);
+// const student = {
+// 	name: 'Nguyễn Văn A',
+//     gender: 'girl'
+// };
+// console.log(student);
+// student['name'] = 'Trần Thị B';
+// //student.name = 'Trần Thị B';
+// student.age = 15;
+// console.log(student);
 
-delete student.gender;
-console.log(student);
+// delete student.gender;
+// console.log(student);
+
+// const listStudent = [
+// 	{
+// 		name: 'Nguyễn Văn A',
+// 		gender: 'girl',
+// 	},
+// 	{
+// 		name: 'Trần Thị B',
+// 		gender: 'girl',
+// 		skills: ['C++', 'Python'],
+// 	},
+// ];
+// console.log(listStudent[1].skills[0]);
+
+// Duyệt object
+const student = {
+	name: 'Trần Thị B',
+	gender: 'girl',
+	skills: ['C++', 'Python'],
+};
+// C1
+// console.log(student.name);
+// console.log(student.gender);
+// console.log(student.skills);
+// C2: for in
+for(let abc in student){
+    console.log(student[abc])
+}
